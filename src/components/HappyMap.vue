2eb698cdfc6c4c1e1e409693bfd85ad2f9e2ca62
Rentoo <dreamyiz@outlook.com>
2019-08-27 22:29:26 +0800

    new map 方式引入

--- src/components/HappyMap.vue
+++ src/components/HappyMap.vue
@@ -1,35 +1,31 @@
 <template>
   <div class="happy-map">
-    <baidu-map
-      :center="center"
-      :zoom="zoom"
-      @ready="handler"
-      class="map"
-      ak="D74e2300cc18a93aa99b8715eeb122e5"
-    ></baidu-map>
+    <div id="allmap"></div>
   </div>
 </template>
 
 <script>
-import BaiduMap from "vue-baidu-map/components/map/Map.vue";
-
 export default {
   name: "HappyMap",
-  components: {
-    BaiduMap
-  },
-  props: {},
   data() {
     return {
-      center: { lng: 0, lat:  0},
+      center: { lng: 0, lat: 0 },
       zoom: 3
     };
+  },
+  mounted() {
+    this.initBMap();
   },
   methods: {
     handler() {
       this.center = "深圳市";
       this.zoom = 14;
-    }    
+    },
+    initBMap() {
+      /*eslint-disable */
+      var map = new BMap.Map("allmap"); // 创建地图实例 
+      map.centerAndZoom("深圳市", 13); // 初始化地图，设置中心点坐标和地图级别
+    }
   }
 };
 </script>
@@ -37,7 +33,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 .happy-map
-  .map 
+  #allmap 
     width: 100vw
     height: 100vh
 </style>
