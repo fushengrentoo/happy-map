4e844f1d55fdb0346918f813049aed9ddc756a2c
Rentoo Windows10 <yaolongliang@everest-fortune.com>
2019-08-27 16:35:20 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HappyMap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HappyMap from './components/HappyMap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HappyMap
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass" scoped>
+*
+  margin: 0
+  padding: 0
 </style>
+
---
+++ src/components/HappyMap.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="happy-map">
+    <baidu-map
+      :center="center"
+      :zoom="zoom"
+      @ready="handler"
+      class="map"
+      ak="D74e2300cc18a93aa99b8715eeb122e5"
+    ></baidu-map>
+  </div>
+</template>
+
+<script>
+import BaiduMap from "vue-baidu-map/components/map/Map.vue";
+
+export default {
+  name: "HappyMap",
+  components: {
+    BaiduMap
+  },
+  props: {},
+  data() {
+    return {
+      center: { lng: 0, lat: 0 },
+      zoom: 3
+    };
+  },
+  methods: {
+    handler() {
+      this.center.lng = 116.404;
+      this.center.lat = 39.915;
+      this.zoom = 15;
+    }    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+.happy-map
+  .map 
+    width: 100vw
+    height: 100vh
+</style>
